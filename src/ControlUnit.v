ac448cfef8f3a23046b80b1deb9dcb5e0bef3efa
Shaza-Is <shaza993@yahoo.com>
2015-12-25 13:43:11 +0200

    Control unit delays in the recomended format

--- src/ControlUnit.v
+++ src/ControlUnit.v
@@ -22,95 +22,88 @@ module ControlUnit(Opcode,RegDstn,Branch,MemRead,MemtoReg,ALUop,MemWrite,ALUsrc,
 	begin
 		case(Opcode)
 		R: begin
-			#2 
-			RegDstn = 2'b01; //rd
-			Branch = 0;
-			MemRead = 0;
-			MemtoReg = 2'b00;
-			ALUop = 2'b10; //1	.. funct: add: 100000, subtract: 100010, AND: 100100, OR: 100101, SLT: 101010
-			MemWrite = 0;
-			ALUsrc = 0;
-			RegWrite = 1;
-			Jump = 0;
-			Arith = 1'bx;
+			RegDstn <= #2 2'b01; //rd
+			Branch <= #2 0;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'b00;
+			ALUop <= #2 2'b10; //1	.. funct: add: 100000, subtract: 100010, AND: 100100, OR: 100101, SLT: 101010
+			MemWrite <= #2 0;
+			ALUsrc <= #2 0;
+			RegWrite <= #2 1;
+			Jump <= #2 0;
+			Arith <= #2 1'bx;
 		end
 		Addi: begin
-#2
-			RegDstn = 2'b00; //rt
-			Branch = 0;
-			MemRead = 0;
-			MemtoReg = 2'b00;
-			ALUop = 2'b00; //00 for adding
-			MemWrite = 0;
-			ALUsrc = 1;
-			RegWrite = 1;
-			Jump = 0;
-			Arith = 1;
+			RegDstn <= #2 2'b00; //rt
+			Branch <= #2 0;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'b00;
+			ALUop <= #2 2'b00; //00 for adding
+			MemWrite <= #2 0;
+			ALUsrc <= #2 1;
+			RegWrite <= #2 1;
+			Jump <= #2 0;
+			Arith <= #2 1;
 		end
 		Lw: begin
-#2
-			RegDstn = 2'b00;
-			Branch = 0;
-			MemRead = 1;
-			MemtoReg = 2'b01;
-			ALUop = 2'b00; //00  for lw
-			MemWrite = 0;
-			ALUsrc = 1;
-			RegWrite = 1;
-			Jump = 0;
-			Arith = 1'b1;
+			RegDstn <= #2 2'b00;
+			Branch <= #2 0;
+			MemRead <= #2 1;
+			MemtoReg <= #2 2'b01;
+			ALUop <= #2 2'b00; //00  for lw
+			MemWrite <= #2 0;
+			ALUsrc <= #2 1;
+			RegWrite <= #2 1;
+			Jump <= #2 0;
+			Arith <= #2 1'b1;
 		end
 		Sw: begin
-#2
-			RegDstn = 2'bxx; //x
-			Branch = 0;
-			MemRead = 0;
-			MemtoReg = 2'bxx; //x
-			ALUop = 2'b00; //00 for sw
-			MemWrite = 1;
-			ALUsrc = 1;
-			RegWrite = 0;
-			Jump = 0;
-			Arith = 1;
+			RegDstn <= #2 2'bxx; //x
+			Branch <= #2 0;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'bxx; //x
+			ALUop <= #2 2'b00; //00 for sw
+			MemWrite <= #2 1;
+			ALUsrc <= #2 1;
+			RegWrite <= #2 0;
+			Jump <= #2 0;
+			Arith <= #2 1;
 		end
 		Andi: begin
-#2
-			RegDstn = 2'b00; //rt
-			Branch = 0;
-			MemRead = 0;
-			MemtoReg = 2'b00;
-			ALUop = 2'b11; //11 for and
-			MemWrite = 0;
-			ALUsrc = 1;
-			RegWrite = 1;
-			Jump = 0;
-			Arith = 0;
+			RegDstn <= #2 2'b00; //rt
+			Branch <= #2 0;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'b00;
+			ALUop <= #2 2'b11; //11 for and
+			MemWrite <= #2 0;
+			ALUsrc <= #2 1;
+			RegWrite <= #2 1;
+			Jump <= #2 0;
+			Arith <= #2 0;
 		end
 		Beq: begin
-#2
-			RegDstn = 2'bxx; //x
-			Branch = 1;
-			MemRead = 0;
-			MemtoReg = 2'bxx; //x
-			ALUop = 2'b01; //01 for beq .. zero flag
-			MemWrite = 0;
-			ALUsrc = 0;
-			RegWrite = 0;
-			Jump = 0;
-			Arith = 1;
+			RegDstn <= #2 2'bxx; //x
+			Branch <= #2 1;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'bxx; //x
+			ALUop <= #2 2'b01; //01 for beq .. zero flag
+			MemWrite <= #2 0;
+			ALUsrc <= #2 0;
+			RegWrite <= #2 0;
+			Jump <= #2 0;
+			Arith <= #2 1;
 		end
 		Jal: begin
-#2
-			RegDstn = 2'b10;
-			Branch = 0;
-			MemRead = 0;
-			MemtoReg = 2'b10;
-			ALUop = 2'bxx; //x
-			MemWrite = 0;
-			ALUsrc = 1'bx; //x
-			RegWrite = 1;
-			Jump = 1;
-			Arith = 1'bx;
+			RegDstn <= #2 2'b10;
+			Branch <= #2 0;
+			MemRead <= #2 0;
+			MemtoReg <= #2 2'b10;
+			ALUop <= #2 2'bxx; //x
+			MemWrite <= #2 0;
+			ALUsrc <= #2 1'bx; //x
+			RegWrite <= #2 1;
+			Jump <= #2 1;
+			Arith <= #2 1'bx;
 		end
 		endcase
 	end
